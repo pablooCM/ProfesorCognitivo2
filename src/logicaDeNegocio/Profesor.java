8d3f0292200c33dc991dbb09e51caef2c9b904a2
pablooCM <jopablo13@gmail.com>
2017-05-02 20:54:36 -0500

    Subo nuevas clases para consistencia
    Estudiante, Profesor, Usuario

---
+++ src/logicaDeNegocio/Profesor.java
@@ -0,0 +1,12 @@
+package logicaDeNegocio;
+import dto.DTO_Usuario;
+
+public class Profesor extends Usuario {
+	protected int identificacionFuncionario;
+	protected String contrasena;
+	
+	public void Autenticacion(){
+		
+	}
+
+}
